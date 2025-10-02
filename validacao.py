8835c0c5fc7cb16a02e7fc85594c307f13ce2afd
Rod12lePotatoLord2 <rodrigo.limabarbosa@upe.brgit config --global user.email rodrigo.limabarbosa@upe.brgit config --global user.email rodrigo.limabarbosa@upe.br>
2025-10-02 16:32:38 -0300

    Remove validacao.py da branch produto-escalar

--- validacao.py
+++
@@ -1,8 +0,0 @@
-def validar_vetores(v1, v2):
-    if not v1 or not v2:
-        raise ValueError("Nenhum dos vetores pode ser vazio.")
-    if len(v1) != len(v2):
-        raise ValueError("Os vetores devem ter o mesmo número de dimensões.")
-    if not all(isinstance(x, (int, float)) for x in v1 + v2):
-        raise ValueError("Todos os elementos devem ser números.")
-    return True
