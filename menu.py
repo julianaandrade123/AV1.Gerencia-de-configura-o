6804dde1bd09b5ab38197a1bb5141112416e4467
Juliana Andrade <Juli24013@gmail.com>
2025-10-02 17:17:39 -0300

    tá aqui

---
+++ menu.py
@@ -0,0 +1,8 @@
+def exibir_menu():
+    print("\n=== Menu de Operações com Vetores ===")
+    print("1 - Produto Misto")
+    print("2 - Produto Escalar")
+    print("3 - Soma/Subtração")
+    print("0 - Sair")
+
+exibir_menu()
