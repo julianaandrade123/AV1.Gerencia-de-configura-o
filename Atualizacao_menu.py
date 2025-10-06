36a0030e10ac1b0a808c2965ada39633a705779f
Rod12lePotatoLord2 <rodrigo.limabarbosa@upe.brgit config --global user.email rodrigo.limabarbosa@upe.brgit config --global user.email rodrigo.limabarbosa@upe.br>
2025-10-06 09:30:26 -0300

    Teste de conflitos

--- Atualizacao_menu.py
+++ Atualizacao_menu.py
@@ -1,7 +1,7 @@
 def exibir_menu():
-    print("\n=== Menu de Operações com Vetores ===") # Adição de = para ficar mais bonito
+    print("\n=== Menu de Operações com Vetores ===")
     print("1 - Produto Misto")
     print("2 - Produto Escalar")
     print("3 - Soma/Subtração")
-    print("4 - Normalizar Vetor")
+    print("4 - Normalização de Vetor")
     print("0 - Sair")
