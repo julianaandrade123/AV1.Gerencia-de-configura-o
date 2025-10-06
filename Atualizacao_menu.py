7c08d7aa68570c23601efdd4a3a6f427be4df0ac
Rod12lePotatoLord2 <rodrigo.limabarbosa@upe.brgit config --global user.email rodrigo.limabarbosa@upe.brgit config --global user.email rodrigo.limabarbosa@upe.br>
2025-10-06 09:37:34 -0300

    Alteração conflitante no Atualizacao_menu.py

--- Atualizacao_menu.py
+++ Atualizacao_menu.py
@@ -3,5 +3,5 @@ def exibir_menu():
     print("1 - Produto Misto")
     print("2 - Produto Escalar")
     print("3 - Soma/Subtração")
-    print("4 - Normalização de Vetor")
+    print("4 - Normalizar os Vetores")
     print("0 - Sair")
