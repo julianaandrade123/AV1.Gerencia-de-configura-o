0c3b2396a5ba5c745f655945699f63f97a3581cd
Rod12lePotatoLord2 <rodrigo.limabarbosa@upe.brgit config --global user.email rodrigo.limabarbosa@upe.brgit config --global user.email rodrigo.limabarbosa@upe.br>
2025-10-06 09:27:02 -0300

    Teste de conflito

--- Atualizacao_menu.py
+++ Atualizacao_menu.py
@@ -1,5 +1,5 @@
 def exibir_menu():
-    print("\n=== Menu de Operações com Vetores ===") # Adição de = para ficar mais bonito visualmente
+    print("\n=== Menu de Operações com Vetores ===") # Adição de = para ficar mais bonito
     print("1 - Produto Misto")
     print("2 - Produto Escalar")
     print("3 - Soma/Subtração")
