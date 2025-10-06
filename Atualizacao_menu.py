fbaa45bc21daf4ad086b0535f1def23bb6f4f339
Juliana Andrade <Juli24013@gmail.com>
2025-10-06 09:10:42 -0300

    Menu para simular conflitos

--- Atualizacao_menu.py
+++ Atualizacao_menu.py
@@ -1,5 +1,5 @@
 def exibir_menu():
-    print("\n=== Menu de Operações com Vetores ===")
+    print("\n Menu de Operações com Vetores")
     print("1 - Produto Misto")
     print("2 - Produto Escalar")
     print("3 - Soma/Subtração")
