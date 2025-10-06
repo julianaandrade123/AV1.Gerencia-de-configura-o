b684938b873ab84a7c8b4a02eb1f3dacf09e7969
Juliana Andrade <Juli24013@gmail.com>
2025-10-06 08:51:58 -0300

    Nova opção menu

---
+++ Atualizacao_menu.py
@@ -0,0 +1,7 @@
+def exibir_menu():
+    print("\n=== Menu de Operações com Vetores ===")
+    print("1 - Produto Misto")
+    print("2 - Produto Escalar")
+    print("3 - Soma/Subtração")
+    print("4 - Normalizar Vetor")
+    print("0 - Sair")
